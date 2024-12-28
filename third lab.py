54866e375142a4e931194e421094eac35107e1fe
Dev Pandey <139095228+dpandey62@users.noreply.github.com>
2024-12-28 19:01:14 +0530

    ANN

---
+++ third lab.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt    
+    
+x = []    
+y = []    
+    
+plt.plot(x,y)    
+    
+plt.title("Line graph")    
+plt.ylabel('Y axis')    
+plt.xlabel('X axis')    
+plt.show()    
